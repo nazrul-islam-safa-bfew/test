63cb1a44cbde4c92c78c47995b475fe1c02058fa
nazrul-islam-safa-bfew <it11@bfew.net>
2021-11-08 12:02:37 +0600

    firtst

--- edit_user_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/db_connect.php';
-$id=$_GET['id'];
-$status=$_GET['status'];
-
-$update_edit="UPDATE users SET status=2 WHERE id='$id' ";
-$a=mysqli_query($db_connect,$update_edit);
-header('location:user_show_list.php');
-?>
